15a90ece82417afb52ae1292eb235b27a441b77a
parkgain <pgi082247@gmail.com>
2023-02-02 16:34:26 +0900

    feat : index.html 파일에 슬랙 css 링크 추가함

---
+++ channel/src/components/ChannelList/index.jsx
@@ -0,0 +1,35 @@
+<>
+  <h2>
+    <CollapseButton collapse={channelCollapse} onClick={toggleChannelCollapse}>
+      <i
+        className="c-icon p-channel_sidebar__section_heading_expand p-channel_sidebar__section_heading_expand--show_more_feature c-icon--caret-right c-icon--inherit c-icon--inline"
+        data-qa="channel-section-collapse"
+        aria-hidden="true"
+      />
+    </CollapseButton>
+    <span>Channels</span>
+  </h2>
+  <div>
+    {!channelCollapse &&
+      channelData?.map((channel) => {
+        const count = countList[`c-${channel.id}`];
+        return (
+          <NavLink
+            key={channel.name}
+            activeClassName="selected"
+            to={`/workspace/${workspace}/channel/${channel.name}`}
+            onClick={resetCount(`c-${channel.id}`)}
+          >
+            <span
+              className={count !== undefined && count >= 0 ? "bold" : undefined}
+            >
+              # {channel.name}
+            </span>
+            {count !== undefined && count > 0 && (
+              <span className="count">{count}</span>
+            )}
+          </NavLink>
+        );
+      })}
+  </div>
+</>;
