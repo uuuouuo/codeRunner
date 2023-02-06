52532304792d572ed3d609e01a0d7f545761d6e1
장소명 <som0ng@naver.com>
2023-02-06 11:22:54 +0900

    feat : 채팅 서비스 추가

---
+++ chat/src/main/java/com/example/chat/ChatService.java
@@ -0,0 +1,14 @@
+package com.example.chat;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class ChatService {
+
+    // 채팅방 생성 후 저장
+//    public Map<String, Object> makeChatRoom() {}
+
+    // 채팅 내역 가져오기 -> 채팅방 id 통해 기록 가져오기
+}
