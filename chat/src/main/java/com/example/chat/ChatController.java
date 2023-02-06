e17c53e3c5dbf08084854dd61ef99c79af3b40a2
장소명 <som0ng@naver.com>
2023-02-06 11:21:50 +0900

    feat: 채팅 컨트롤러 추가

---
+++ chat/src/main/java/com/example/chat/ChatController.java
@@ -0,0 +1,9 @@
+package com.example.chat;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+    // 채팅방 생성 후 저장
+    // 채팅 내역 가져오기 -> 채팅방 id 통해 기록 가져오기
+}
