aa07e76ba6206759bc3b868155f7bac8ccd1abd1
장소명 <som0ng@naver.com>
2023-02-10 18:32:49 +0900

    feat : @EnableCaching 어노테이션 추가

--- chat/src/main/java/com/example/chat/ChatApplication.java
+++ chat/src/main/java/com/example/chat/ChatApplication.java
@@ -2,8 +2,12 @@ package com.example.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCaching
+@EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 public class ChatApplication {
