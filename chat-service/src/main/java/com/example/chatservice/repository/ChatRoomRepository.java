9fc971ac445e3f3d82fec737bc480d0585d3d7d3
uuuouuo <89640705+uuuouuo@users.noreply.github.com>
2023-02-13 15:28:00 +0900

    Revert "Feature/chat"

--- chat-service/src/main/java/com/example/chatservice/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.chatservice.repository;
-
-import com.example.chatservice.model.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
-}
