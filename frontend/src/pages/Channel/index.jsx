c2a52d3f4c8bb4031e1f58e43362454407623c5a
parkgain <pgi082247@gmail.com>
2023-02-20 11:03:50 +0900

    fixes : 게시글 버그 해결 완료!

--- frontend/src/pages/Channel/index.jsx
+++ frontend/src/pages/Channel/index.jsx
@@ -11,6 +11,7 @@ import { channelMemberSelector } from "../../store/channelAtom";
 import ChatList from "../../components/ChatList";
 import axios from "axios";
 import { channelListSelector } from "../../store/channelAtom";
+
 const PAGE_SIZE = 20;
 const Channel = () => {
   const { id } = useParams();
@@ -34,21 +35,23 @@ const Channel = () => {
   }, []);
   const [chatData, setChatData] = useState([]);
   const scrollbarRef = useRef(null);
-  const [chat, onChangeChat] = useInput("");
+  const [chat, onChangeChat, setChat] = useInput("");
   const isEmpty = chatData?.[0]?.length === 0;
   const isReachingEnd =
     isEmpty || (chatData && chatData[chatData.length - 1]?.length < PAGE_SIZE);
-  const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
+  const chatSections = makeSection(chatData ? chatData.reverse() : []);
 
   const loadPosts = async () => {
     try {
       const { data } = await axios.get(
         `http://localhost:8082/channel/${channel_id}/posts/get`
       );
-      console.dir(data);
-      const content = data.map((row) => row.content);
+      const content = data.map((row, index) => row.content);
       setChatData(content);
-      return data;
+      if (scrollbarRef.current) {
+        console.log("scrollToBottom!", scrollbarRef.current?.getValues());
+        scrollbarRef.current.scrollToBottom();
+      }
     } catch (error) {
       console.log(error);
     }
@@ -61,18 +64,20 @@ const Channel = () => {
         user_id: 1,
         content: chat,
       });
+      setChat("");
     } catch (error) {
       console.log(error);
     }
   };
 
-  const onSubmitForm = useCallback((e) => {
-    e.preventDefault();
-    loadPosts();
-    writePosts();
-    loadPosts();
-  }, []);
-
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      setChatData([...chatData, chat]);
+      writePosts();
+    },
+    [chat, channel, chatData]
+  );
   return (
     <Container>
       <Header>
